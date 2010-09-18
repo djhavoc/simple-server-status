6e6413354af5b5ade97782f64d82dee016636b2e
joet3ch <joe@t3ch.com>
2010-09-17 23:54:13 -0400

    mysql check working state

--- Classes/Check.py
+++ Classes/Check.py
@@ -1,3 +1,4 @@
+import copy
 import MySQLdb
 import urllib
 import TimeoutSocket
@@ -10,61 +11,60 @@ class Checks:
         self.db = Database.Connection()
     
     ## build list of services for an inspection
-    def servicesToInspect(self):
-        ## mysql
-        self.db.cursor.execute("""SELECT id, 
-                                            title, 
-                                            db_name, 
-                                            db_host,
-                                            db_user, 
-                                            AES_DECRYPT('db_pass','%s') as db_pass
-                                    FROM services 
-                                    WHERE services.kind = 'mysql'""" %(config.DB_CRYPTOKEY))
-        self.queueMysql = self.db.cursor
-                                                    
-        ## http
-        self.db.cursor.execute("""SELECT id, http_url
-                                                    FROM services 
-                                                    WHERE services.kind = 'http'""")
-        self.queueHttp = self.db.cursor
+    def servicesToInspect(self, kind):
+        if (kind == 'mysql'):
+            print config.DB_CRYPTOKEY
+            self.db.cursor.execute("""SELECT id, 
+                                                title, 
+                                                db_name, 
+                                                db_host,
+                                                db_user, 
+                                                AES_DECRYPT(db_pass,'%s') as db_pass
+                                        FROM services 
+                                        WHERE services.kind = 'mysql'""" %(config.DB_CRYPTOKEY))
+
+        elif (kind == 'http'):
+            ## http
+            self.db.cursor.execute("""SELECT id, http_url
+                                                        FROM services 
+                                                        WHERE services.kind = 'http'""")
 
         
-        ## tcp
-        self.db.cursor.execute("""SELECT id, tcp_ip, tcp_port
-                                                    FROM services 
-                                                    WHERE services.kind = 'tcp'""")
-        self.queueTcp = self.db.cursor
-
-        self.run()
+        elif (kind == 'tcp'):
+            self.db.cursor.execute("""SELECT id, tcp_ip, tcp_port
+                                                        FROM services 
+                                                        WHERE services.kind = 'tcp'""")
 
     ## perform inspections
-    def run(self):
-        ## mysql
-        if (self.queueMysql.rowcount > 0):
-            for item in self.queueMysql.fetchall():
-                #self.mysql(item['id'], item['db_host'], item['db_name'], item['db_user'], item['db_pass'])
-                pass
-        ## http
-        if (self.queueHttp.rowcount > 0):
-            for item in self.queueHttp.fetchall():
-                self.http(item['id'], item['http_url'])        
+    def run(self, kind):
+        if (kind == 'mysql'):
+            if (self.db.cursor.rowcount > 0):
+                for item in self.db.cursor.fetchall():
+                    self.mysql(item['id'], item['db_host'], item['db_name'], item['db_user'], item['db_pass'])
+
+        elif (kind == 'http'):
+            if (self.db.cursor.rowcount > 0):
+                for item in self.db.cursor.fetchall():
+                    self.http(item['id'], item['http_url'])        
         
-        ## tcp
-        if (self.queueTcp.rowcount > 0):
-            for item in self.queueTcp.fetchall():
-                self.tcp(item['id'], item['ip'], item['tcp_port'])        
+        elif (kind == 'tcp'):
+            if (self.db.cursor.rowcount > 0):
+                for item in self.db.cursor.fetchall():
+                    self.tcp(item['id'], item['ip'], item['tcp_port'])        
 
         return True
         
     def recordResult(self, status, serviceID):
-        self.db.cursor.execute("""INSERT INTO results
-                                    SET when = NOW(), status = '%s'
-                                    WHERE services_id = %s
-                                """ % (status, serviceID))
-        return db.cursor.lastrowid
+        self.db.cursor.execute("""UPDATE results SET last_check = NOW(), status = '%s' WHERE services_id = %s """ % (status, serviceID))
+        return self.db.cursor.lastrowid
     
     ## check a mysql connection
     def mysql(self, serviceID, host, name, user, passwd):
+        print serviceID
+        print host
+        print name
+        print user
+        print passwd
         try:
             dbConn = MySQLdb.connect (host = host,
                                     user = user,
@@ -73,6 +73,7 @@ class Checks:
             status = 'good'
         except MySQLdb.Error, e:
             status = 'bad'
+        print status
         self.recordResult(status, serviceID)
                     
     ## check a web service
--- launch_checks.py
+++ launch_checks.py
@@ -10,7 +10,15 @@
 import sys
 sys.path.append('Classes/')
 import Check
-
+ 
 if __name__ == "__main__":
     launcher = Check.Checks()
-    launcher.servicesToInspect()
+
+    launcher.servicesToInspect('mysql')
+    launcher.run('mysql')
+
+    launcher.servicesToInspect('http')
+    launcher.run('http')
+
+    launcher.servicesToInspect('tcp')
+    launcher.run('tcp')
